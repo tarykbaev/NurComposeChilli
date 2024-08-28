5f95835ae83cc131798b7c7ca44d86f1ba3685c5
kulanbekova <ulanbekova_ka@auca.kg>
2024-08-28 07:29:18 +0600

    Bottom Sheet

---
+++ composeChili/src/main/java/com/design/composechili/components/bottom_sheet/ActionBottomSheet.kt
@@ -0,0 +1,83 @@
+package com.design.composechili.components.bottom_sheet
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.design.composechili.components.buttons.BaseButton
+import com.design.composechili.components.buttons.ChiliButtonStyle
+import com.design.composechili.theme.ChiliAttribute
+import com.design.composechili.theme.ChiliTextStyle
+import com.design.composechili.theme.ChiliTheme
+import kotlinx.coroutines.launch
+
+enum class ActionBottomSheetButtonType {
+    SIMPLE, ACCENT
+}
+
+data class ActionBottomSheetButton(
+    val title: String?,
+    val type: ActionBottomSheetButtonType,
+    val hideBottomSheetOnClick: Boolean = true,
+    val onClick: (() -> Unit)? = null
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ActionBottomSheet(
+    sheetState: BottomSheetScaffoldState,
+    buttons: List<ActionBottomSheetButton>,
+    content: @Composable () -> Unit
+) {
+    BaseBottomSheet(
+        sheetState = sheetState,
+        bottomSheetContent = {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                buttons.forEach { button ->
+                    ActionButton(button, sheetState)
+                }
+            }
+        }
+    ) {
+        content()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ActionButton(
+    button: ActionBottomSheetButton,
+    sheetState: BottomSheetScaffoldState
+) {
+    val scope = rememberCoroutineScope()
+
+    val buttonTextColor = when (button.type) {
+        ActionBottomSheetButtonType.SIMPLE -> ChiliTheme.Colors.chiliSecondaryTextColor
+        ActionBottomSheetButtonType.ACCENT -> ChiliTheme.Colors.ChiliComponentButtonTextColorActive
+    }
+
+    BaseButton(
+        onClick = {
+            button.onClick?.invoke()
+            if (button.hideBottomSheetOnClick) scope.launch {
+                sheetState.bottomSheetState.hide()
+            }
+        },
+        title = button.title ?: "",
+        buttonStyle = ChiliButtonStyle.Secondary,
+        titleStyle = ChiliTextStyle.get(
+            textSize = ChiliTheme.Attribute.ChiliTextDimensions.TextSizeH8,
+            color = buttonTextColor,
+            font = ChiliAttribute.getDefault().ChiliBoldTextFont
+        ),
+    )
+}
---
+++ composeChili/src/main/java/com/design/composechili/components/bottom_sheet/BaseBottomSheet.kt
@@ -0,0 +1,44 @@
+package com.design.composechili.components.bottom_sheet
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.BottomSheetScaffoldState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.unit.dp
+import com.design.composechili.R
+import com.design.composechili.theme.ChiliTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BaseBottomSheet(
+    sheetState: BottomSheetScaffoldState,
+    bottomSheetContent: @Composable () -> Unit,
+    content: @Composable () -> Unit
+) {
+    BottomSheetScaffold(
+        sheetContent = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(ChiliTheme.Colors.chiliSurfaceBackground)
+            ) {
+                bottomSheetContent()
+            }
+        },
+        scaffoldState = sheetState,
+        sheetShape = RoundedCornerShape(
+            topEnd = dimensionResource(R.dimen.padding_8dp),
+            topStart = dimensionResource(R.dimen.padding_8dp)
+        ),
+        sheetDragHandle = {}, // hides slider
+        sheetPeekHeight = 0.dp // hides slider
+    ) {
+        content()
+    }
+}
