f4c258d1db1e680fd856a7ec8e2514392f796eda
stashmamatov <samtashmamatov7@gmail.com>
2024-09-06 10:53:04 +0600

    finished InAppPush

--- composeChili/src/main/java/com/design/composechili/components/inAppPush/InfoInAppPush.kt
+++ composeChili/src/main/java/com/design/composechili/components/inAppPush/InfoInAppPush.kt
@@ -1,18 +1,24 @@
 package com.design.composechili.components.inAppPush
 
-import androidx.compose.foundation.layout.BoxScopeInstance.align
-import androidx.compose.foundation.layout.FlowRowScopeInstance.align
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CornerBasedShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
 import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
 import com.design.composechili.R
 import com.design.composechili.components.buttons.baseButton.BaseButton
 import com.design.composechili.components.buttons.baseButton.ChiliButtonStyle
@@ -25,6 +31,7 @@ fun InfoInAppPush(
     description: String,
     buttonText: String,
     banner: String = String(),
+    bannerCorner: CornerBasedShape = RoundedCornerShape(24.dp),
     titleStyle: TextStyle = ChiliTextStyle.get(
         textSize = ChiliTheme.Attribute.ChiliTextDimensions.TextSizeH7,
         color = ChiliTheme.Colors.ChiliPrimaryTextColor,
@@ -39,22 +46,36 @@ fun InfoInAppPush(
 ) {
 
     BaseInAppPush(
-        modifier = Modifier.padding(horizontal = 8.dp),
-        onDismissRequest = onDismissRequest
+        modifier = Modifier.padding(horizontal = 8.dp), onDismissRequest = onDismissRequest
     ) {
 
-        if (banner.isNotBlank()) {
-            SubcomposeAsyncImage(
-                modifier = Modifier,
-                model = banner,
-                alignment = Alignment.Center,
-                contentDescription = String()
-            ) {
-                val state = painter.state
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+        ) {
+            if (banner.isNotBlank()) {
+                SubcomposeAsyncImage(
+                    modifier = Modifier.align(Alignment.Center),
+                    model = banner,
+                    alignment = Alignment.Center,
+                    contentScale = ContentScale.Fit,
+                    contentDescription = String()
+                ) {
+                    val state = painter.state
 
-                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                    if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                        CircularProgressIndicator()
+                    } else {
+                        SubcomposeAsyncImageContent(
+                            Modifier.clip(bannerCorner),
+                            clipToBounds = false
+                        )
+                    }
+                }
             }
         }
+
         Text(
             modifier = Modifier.padding(top = 16.dp),
             text = title,
@@ -62,9 +83,7 @@ fun InfoInAppPush(
 
             )
         Text(
-            modifier = Modifier.padding(top = 16.dp),
-            text = description,
-            style = descriptionStyle
+            modifier = Modifier.padding(top = 16.dp), text = description, style = descriptionStyle
         )
         BaseButton(
             modifier = Modifier.padding(top = 16.dp),
