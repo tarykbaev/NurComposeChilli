3b154cac6d0a8cf1bf7e75c870f93e7d0f6c4156
Albert <azhumaev@nurtelecom.kg>
2024-09-16 20:38:31 +0600

    added Snackbar Screen for review all snackbar in app

--- app/src/main/java/com/design/composeChilli/MainActivity.kt
+++ app/src/main/java/com/design/composeChilli/MainActivity.kt
@@ -6,22 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
-import com.design.composeChilli.screen.ButtonsScreen
-import com.design.composeChilli.screen.TextAppearanceScreen
+import com.design.composeChilli.screen.SnackbarScreen
 import com.design.composechili.components.topAppBar.ChiliBaseTopAppBar
 import com.design.composechili.components.topAppBar.ChiliBaseTopAppBarParams
 import com.design.composechili.theme.ChiliTheme
 
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -37,10 +34,14 @@ class MainActivity : ComponentActivity() {
                         title = "NurComposeChili",
                         isDividerVisible = false,
                         endIcon = R.drawable.ic_dark_mode,
-                        params = ChiliBaseTopAppBarParams.Default.copy(endIconColorFilter = ColorFilter.tint(ChiliTheme.Colors.ChiliPrimaryTextColor)),
+                        params = ChiliBaseTopAppBarParams.Default.copy(
+                            endIconColorFilter = ColorFilter.tint(
+                                ChiliTheme.Colors.ChiliPrimaryTextColor
+                            )
+                        ),
                         onEndIconClick = { isDarkModeEnable = !isDarkModeEnable }
                     )
-                    ButtonsScreen()
+                    SnackbarScreen()
                 }
                 /*ComponentSelectorScreen(
                     onTextAppearanceItemClicked = {},
