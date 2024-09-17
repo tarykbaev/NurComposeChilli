f5c7a747ec5078b526c9c7e25b45853ff1c3d7a3
Bektur Aitbaev <baitbaev@in.nurtelecom.kg>
2024-09-17 17:12:48 +0600

    InputFields Screen create

--- composeChili/src/main/java/com/design/composechili/components/input/baseInput/BaseInput.kt
+++ composeChili/src/main/java/com/design/composechili/components/input/baseInput/BaseInput.kt
@@ -15,18 +15,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.design.composechili.R
-import com.design.composechili.theme.ChiliTextStyle
-import com.design.composechili.theme.ChiliTheme
 
 @Composable
 fun BaseInput(
@@ -40,7 +35,7 @@ fun BaseInput(
     @DrawableRes startIcon: Int? = null,
     @DrawableRes endIcon: Int? = null
 ) {
-    Row {
+    Row (verticalAlignment = Alignment.CenterVertically){
         if (startIcon != null) {
             Image(
                 painter = painterResource(id = startIcon),
@@ -57,6 +52,7 @@ fun BaseInput(
         TextField(
             modifier = modifier
                 .wrapContentSize()
+                .weight(1f)
                 .fillMaxWidth()
                 .padding(params.textFieldPadding),
             value = textFieldValue,
