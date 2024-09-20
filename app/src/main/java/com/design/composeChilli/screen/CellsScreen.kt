2091d58ba9dd8c2d0d913822d24aad962da096a8
Albert <azhumaev@nurtelecom.kg>
2024-09-20 12:24:32 +0600

    process

--- app/src/main/java/com/design/composeChilli/screen/CellsScreen.kt
+++ app/src/main/java/com/design/composeChilli/screen/CellsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -27,7 +28,8 @@ fun CellsScreen() {
         ) {
             Spacer(modifier = Modifier.size(12.dp))
             BaseCell(
-                modifier = Modifier,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp),
                 title = "BaseCell",
                 subtitle = "ChevronVisible",
                 isChevronVisible = true,
--- composeChili/src/main/java/com/design/composechili/components/cell/baseCell/BaseCell.kt
+++ composeChili/src/main/java/com/design/composechili/components/cell/baseCell/BaseCell.kt
@@ -3,6 +3,8 @@ package com.design.composechili.components.cell.baseCell
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,9 +13,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -46,11 +50,23 @@ fun BaseCell(
     isDividerVisible: Boolean = false,
     @DrawableRes startIcon: Int? = null,
     baseCellParams: BaseCellParams = BaseCellParams.Default,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    onClick: (() -> Unit)? = null,
 ) {
     Box(
         modifier
             .clip(baseCellParams.cornerMode.toRoundedShape())
             .background(baseCellParams.background)
+
+            .clickable(
+                onClick = {
+                    onClick?.invoke()
+                },
+                interactionSource = interactionSource,
+                indication = rememberRipple(
+                    bounded = false,
+                )
+            )
     ) {
         Row(
             Modifier
