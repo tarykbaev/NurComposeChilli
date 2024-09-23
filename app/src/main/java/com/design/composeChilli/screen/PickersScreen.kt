3cf4e57379cb17ccaaa29e8e302d40b5588bf5d8
Bektur Aitbaev <baitbaev@in.nurtelecom.kg>
2024-09-23 15:50:48 +0600

    Remove TextPicker

--- app/src/main/java/com/design/composeChilli/screen/PickersScreen.kt
+++ app/src/main/java/com/design/composeChilli/screen/PickersScreen.kt
@@ -1,41 +1,26 @@
 package com.design.composeChilli.screen
 
 import android.os.Build
-import android.view.Gravity
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
-import androidx.compose.ui.window.DialogWindowProvider
 import com.design.composechili.components.buttons.baseButton.BaseButton
 import com.design.composechili.components.picker.chiliDatePicker.ChiliDatePickerDialog
 import com.design.composechili.components.picker.chiliDatePicker.ChiliDatePickerParams
 import com.design.composechili.components.picker.chiliDatePicker.DatePickerTimeParams
 import com.design.composechili.components.picker.chiliTimePicker.ChiliTimePickerDialog
-import com.design.composechili.components.picker.chiliWheelPicker.ChiliWheelTextPicker
-import com.design.composechili.theme.ChiliTextStyle
 import com.design.composechili.theme.ChiliTheme
 import java.time.LocalDateTime
 
@@ -46,7 +31,6 @@ fun PickersScreen() {
     var showDateRangePickerDialog by remember { mutableStateOf(false) }
     var showDatePickerDialog by remember { mutableStateOf(false) }
     var showTimePickerDialog by remember { mutableStateOf(false) }
-    var showTextPicker by remember { mutableStateOf(false) }
 
     Box(modifier = Modifier
         .fillMaxSize()
@@ -68,11 +52,6 @@ fun PickersScreen() {
             BaseButton(
                 onClick = { showTimePickerDialog = true },
                 title = "ChiliTimePicker"
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            BaseButton(
-                onClick = { showTextPicker = true },
-                title = "ChiliWheelTextPicker"
             )
         }
         if (showDateRangePickerDialog) {
@@ -130,52 +109,6 @@ fun PickersScreen() {
                     showTimePickerDialog = false
                 }
             )
-        }
-        if (showTextPicker) {
-            Dialog(
-                onDismissRequest = { { /*TODO*/ } },
-                properties = DialogProperties(usePlatformDefaultWidth = false)
-            ) {
-                val dialogWindowProvider = LocalView.current.parent as DialogWindowProvider
-                dialogWindowProvider.window.setGravity(Gravity.BOTTOM)
-
-                Card(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .wrapContentHeight()
-                        .padding(16.dp),
-                    colors = CardDefaults.cardColors(containerColor = ChiliTheme.Colors.ChiliSurfaceBackground),
-                    shape = RoundedCornerShape(16.dp),
-                ) {
-                    Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.spacedBy(8.dp)
-                    ) {
-                        Text(
-                            modifier = Modifier.padding(top = 16.dp),
-                            text = "TextPicker",
-                            style = ChiliTextStyle.get(
-                                color = ChiliTheme.Colors.ChiliPrimaryTextColor
-                            )
-                        )
-                        ChiliWheelTextPicker(
-                            modifier = Modifier.background(color = ChiliTheme.Colors.chiliScreenBackground),
-                            texts = listOf("Text 1", "Text 2", "Text 3", "Text 4", "Text 5"),
-                            rowCount = 1,
-                            style = ChiliTextStyle.get(
-                                textSize = ChiliTheme.Attribute.ChiliTextDimensions.TextSizeH7,
-                                color = ChiliTheme.Colors.ChiliPrimaryTextColor
-                            )
-                        )
-                        BaseButton(
-                            modifier = Modifier.padding(bottom = 16.dp),
-                            onClick = { showTextPicker = false },
-                            title = "Готово"
-                        )
-                    }
-                }
-            }
         }
     }
 }
