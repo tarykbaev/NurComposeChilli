a966c54e0efd6f1c242f99f98a4dc9c17cf2ccff
Bektur Aitbaev <baitbaev@in.nurtelecom.kg>
2024-09-26 11:04:27 +0600

    Grouping container dragging enabled state add

--- app/src/main/java/com/design/composeChilli/screen/GroupingContainerScreen.kt
+++ app/src/main/java/com/design/composeChilli/screen/GroupingContainerScreen.kt
@@ -1,21 +1,26 @@
 package com.design.composeChilli.screen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.design.composeChilli.utils.dragContainer
 import com.design.composeChilli.utils.draggableItems
 import com.design.composeChilli.utils.rememberDragDropState
+import com.design.composechili.components.buttons.baseButton.BaseButton
 import com.design.composechili.components.cell.baseCell.BaseCell
 import com.design.composechili.components.cell.model.CellCornerMode
 import com.design.composechili.theme.ChiliTheme
@@ -33,41 +38,54 @@ fun GroupingContainerScreen() {
         )
     }
     val stateList = rememberLazyListState()
+    var isDraggingEnabled by remember { mutableStateOf(false) }
 
     val dragDropState =
         rememberDragDropState(
-            lazyListState = stateList,
+            lazyListState = if (isDraggingEnabled) stateList else rememberLazyListState(),
             items = list1,
             onMove = { fromIndex, toIndex, sublistIndex -> })
 
-    LazyColumn(
-        modifier = Modifier
-            .dragContainer(dragDropState)
-            .fillMaxWidth(),
-        state = stateList,
-        contentPadding = PaddingValues(16.dp),
-    ) {
-        draggableItems(list1, dragDropState) { modifier, subListIndex, index, value ->
-            val isSingleItem = list1[subListIndex].size == 1
-            val isFirstItem = index == 0
-            val isLastItem = index == list1[subListIndex].lastIndex
-            BaseCell(
-                modifier = modifier,
-                title = value,
-                isChevronVisible = true,
-                isDividerVisible = !isSingleItem && !isLastItem,
-                cellCornerMode = when {
-                    isSingleItem -> CellCornerMode.Single
-                    isFirstItem -> CellCornerMode.Top
-                    isLastItem -> CellCornerMode.Bottom
-                    else -> CellCornerMode.Middle
+    Box {
+        LazyColumn(
+            modifier = Modifier
+                .dragContainer(dragDropState)
+                .fillMaxWidth(),
+            state = if (isDraggingEnabled) stateList else rememberLazyListState(),
+            contentPadding = PaddingValues(16.dp),
+        ) {
+            draggableItems(list1, dragDropState) { modifier, subListIndex, index, value ->
+                val isSingleItem = list1[subListIndex].size == 1
+                val isFirstItem = index == 0
+                val isLastItem = index == list1[subListIndex].lastIndex
+                BaseCell(
+                    modifier = modifier,
+                    title = if (isDraggingEnabled) "Dragging item $value" else value,
+                    isChevronVisible = true,
+                    isDividerVisible = !isSingleItem && !isLastItem,
+                    cellCornerMode = when {
+                        isSingleItem -> CellCornerMode.Single
+                        isFirstItem -> CellCornerMode.Top
+                        isLastItem -> CellCornerMode.Bottom
+                        else -> CellCornerMode.Middle
+                    }
+                )
+                if (isLastItem) {
+                    Spacer(modifier = Modifier.height(16.dp))
                 }
-            )
-            if (isLastItem) {
-                Spacer(modifier = Modifier.height(16.dp))
             }
+            item { Spacer(modifier = Modifier.height(48.dp)) }
         }
+        BaseButton(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .padding(16.dp),
+            onClick = { isDraggingEnabled = !isDraggingEnabled },
+            title = "DraggingEnabled"
+        )
     }
+
 }
 
 @Composable
