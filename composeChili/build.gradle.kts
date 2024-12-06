6ba796bc3c536762de38653655653d8252481577
stashmamatov <samtashmamatov7@gmail.com>
2024-12-06 15:46:49 +0600

    fixed bug jitpack repo

--- composeChili/build.gradle.kts
+++ composeChili/build.gradle.kts
@@ -74,7 +74,7 @@ dependencies {
 afterEvaluate {
     publishing {
         publications {
-            create<MavenPublication>("release") {
+            create<MavenPublication>("maven") {
                 from(components["release"])
                 groupId = "kg.nurtelecom"
                 artifactId = "compose-chili"
