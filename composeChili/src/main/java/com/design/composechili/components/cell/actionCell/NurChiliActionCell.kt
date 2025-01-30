04f4d56aee445eb307e682c57ddd04d95d59e58d
stashmamatov <samtashmamatov7@gmail.com>
2025-01-30 12:25:04 +0600

    refactored code

--- composeChili/src/main/java/com/design/composechili/components/cell/actionCell/NurChiliActionCell.kt
+++ composeChili/src/main/java/com/design/composechili/components/cell/actionCell/NurChiliActionCell.kt
@@ -7,12 +7,9 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -48,8 +45,8 @@ fun NurChiliActionCell(
     params: ActionCellParams = ActionCellParams.Default,
     cellCornerMode: CellCornerMode = CellCornerMode.Single,
     isActionEnabled: Boolean = true,
-    onClick: () -> Unit = {},
     onActionClick: (() -> Unit)? = null,
+    onClick: () -> Unit = {}
 ) {
     val interactionSource = remember { MutableInteractionSource() }
 
