2bd508c0f669bf9db469a0d1356af125c8dc6893
vikim <vikim@nurtelecom.kg>
2025-04-28 21:02:24 +0600

    added cards expandable with lists of services

--- composeChili/src/main/java/com/design/composechili/utils/StringExtension.kt
+++ composeChili/src/main/java/com/design/composechili/utils/StringExtension.kt
@@ -1,14 +1,35 @@
 package com.design.composechili.utils
 
 import android.util.Log
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 
-fun String.safeTake(n:Int): String {
-    Log.e("TAG", "$n & ${this.length}", )
-    return when{
+fun String.safeTake(n: Int): String {
+    Log.e("TAG", "$n & ${this.length}")
+    return when {
         isNotBlank() && length > n -> take(n).plus("...")
         isNotBlank() -> take(n)
         else -> this
+    }
+}
+
+fun Float.addCurrency(): AnnotatedString {
+    return buildAnnotatedString {
+        append("${this@addCurrency} ")
+        withStyle(SpanStyle(textDecoration = TextDecoration.Underline)) {
+            append("с")
+        }
+    }
+}
+
+fun Double.addCurrency(): AnnotatedString {
+    return buildAnnotatedString {
+        append("${this@addCurrency} ")
+        withStyle(SpanStyle(textDecoration = TextDecoration.Underline)) {
+            append("с")
+        }
     }
 }
