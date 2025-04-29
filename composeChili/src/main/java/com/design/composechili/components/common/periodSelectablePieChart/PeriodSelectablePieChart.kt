241826e1af60c756015c69ae09325c233e57a4a2
vikim <vikim@nurtelecom.kg>
2025-04-29 17:44:54 +0600

    working expanding animation

--- composeChili/src/main/java/com/design/composechili/components/common/periodSelectablePieChart/PeriodSelectablePieChart.kt
+++ composeChili/src/main/java/com/design/composechili/components/common/periodSelectablePieChart/PeriodSelectablePieChart.kt
@@ -1,6 +1,7 @@
 package com.design.composechili.components.common.periodSelectablePieChart
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -22,21 +24,26 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import com.design.composechili.R
 import com.design.composechili.components.bottomSheet.actionBottomSheet.ActionBottomSheetContent
@@ -253,77 +260,96 @@ fun PeriodSelectablePieChart_Preview() {
                             .background(Color.White),
                     ) {
                         uiState.value.detalizationInfo.category?.let { category ->
+                            val expandedContentHeight = remember { mutableStateOf(0.dp) }
+                            val localDensity = LocalDensity.current
                             category.forEach { item ->
                                 key(item.hashCode()) {
                                     Row(
                                         modifier = Modifier
+                                            .onSizeChanged { size ->
+                                                expandedContentHeight.value =
+                                                    with(localDensity) { size.height.toDp() }
+                                            }
                                             .clickable { expandedState.value = !expandedState.value }
                                             .padding(dimensionResource(R.dimen.padding_12dp))
                                             .fillMaxWidth(),
                                         verticalAlignment = Alignment.CenterVertically,
                                         horizontalArrangement = Arrangement.Start
                                     ) {
-                                        Canvas(
-                                            modifier = Modifier
-                                                .padding(horizontal = dimensionResource(R.dimen.padding_12dp))
-                                                .size(dimensionResource(R.dimen.view_12dp))
-                                        ) {
-                                            drawCircle(Color.Green)
+                                        Column(Modifier.fillMaxHeight()) {
+                                            Canvas(
+                                                modifier = Modifier
+                                                    .padding(horizontal = dimensionResource(R.dimen.padding_12dp))
+                                                    .size(
+                                                        DpSize(
+                                                            width = dimensionResource(R.dimen.view_12dp),
+                                                            height = if (expandedState.value) expandedContentHeight.value
+                                                            else dimensionResource( R.dimen.view_12dp )
+                                                        )
+                                                    )
+                                                    .animateContentSize()
+                                            ) { drawCircle(Color.Green) }
                                         }
-                                        Text(modifier = Modifier.weight(1f), text = item.name ?: "")
-                                        Text(
-                                            text = item.totalCharge?.addCurrency()
-                                                ?: buildAnnotatedString { append("") },
-                                            color = ChiliTheme.Colors.ChiliValueTextColor
-                                        )
+                                        Column {
+                                            Row {
+                                                Text(modifier = Modifier.weight(1f), text = item.name ?: "")
+                                                Text(
+                                                    text = item.totalCharge?.addCurrency()
+                                                        ?: buildAnnotatedString { append("") },
+                                                    color = ChiliTheme.Colors.ChiliValueTextColor
+                                                )
+                                            }
+                                            AnimatedVisibility(expandedState.value) {
+                                                Column {
+                                                    item.subCategories?.let {
+                                                        it.forEach { subCategory ->
+                                                            Column(
+                                                                modifier = Modifier
+                                                                    .padding(
+                                                                        top = dimensionResource(R.dimen.padding_12dp),
+                                                                        end = dimensionResource(R.dimen.padding_12dp),
+                                                                    )
+                                                                    .fillMaxWidth(),
+                                                            ) {
+                                                                Row {
+                                                                    Text(
+                                                                        modifier = Modifier.weight(1f),
+                                                                        text = subCategory.name ?: ""
+                                                                    )
+                                                                    Text(
+                                                                        text = subCategory.charge?.addCurrency()
+                                                                            ?: buildAnnotatedString { append("") },
+                                                                        color = ChiliTheme.Colors.ChiliValueTextColor
+                                                                    )
+                                                                }
+                                                                Row {
+                                                                    Text(
+                                                                        modifier = Modifier
+                                                                            .weight(1f)
+                                                                            .padding(top = dimensionResource(R.dimen.padding_8dp)),
+                                                                        text = subCategory.getPaymentDate()
+                                                                    )
+                                                                }
+                                                            }
+                                                            HorizontalDivider(
+                                                                modifier = Modifier.padding(
+                                                                    top = dimensionResource(
+                                                                        R.dimen.padding_4dp
+                                                                    )
+                                                                ),
+                                                                color = ChiliTheme.Colors.ChiliDividerColor
+                                                            )
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
                                     }
                                     if (uiState.value.detalizationInfo.category?.last() != item)
                                         HorizontalDivider(
                                             modifier = Modifier.padding(start = dimensionResource(R.dimen.padding_48dp)),
                                             color = ChiliTheme.Colors.ChiliDividerColor
                                         )
-                                    AnimatedVisibility(expandedState.value) {
-                                        item.subCategories?.let {
-                                            it.forEach { subCategory ->
-                                                Column(
-                                                    modifier = Modifier
-                                                        .padding(
-                                                            top = dimensionResource(R.dimen.padding_12dp),
-                                                            start = dimensionResource(R.dimen.padding_48dp),
-                                                            end = dimensionResource(R.dimen.padding_12dp),
-                                                        )
-                                                        .fillMaxWidth(),
-                                                ) {
-                                                    Row {
-                                                        Text(
-                                                            modifier = Modifier.weight(1f),
-                                                            text = subCategory.name ?: ""
-                                                        )
-                                                        Text(
-                                                            text = subCategory.charge?.addCurrency()
-                                                                ?: buildAnnotatedString { append("") },
-                                                            color = ChiliTheme.Colors.ChiliValueTextColor
-                                                        )
-                                                    }
-                                                    Row {
-                                                        Text(
-                                                            modifier = Modifier
-                                                                .weight(1f)
-                                                                .padding(top = dimensionResource(R.dimen.padding_8dp)),
-                                                            text = subCategory.getPaymentDate()
-                                                        )
-                                                    }
-                                                }
-                                            }
-                                        }
-                                        HorizontalDivider(
-                                            modifier = Modifier.padding(
-                                                top = dimensionResource(R.dimen.padding_4dp),
-                                                start = dimensionResource(R.dimen.padding_48dp)
-                                            ),
-                                            color = ChiliTheme.Colors.ChiliDividerColor
-                                        )
-                                    }
                                 }
                             }
                         }
